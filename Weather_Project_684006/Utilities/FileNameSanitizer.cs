f3e3363cf55bee39242f5beec30bf1073d9a6f12
Rodrigo Bange <89969052+RodrigoBange@users.noreply.github.com>
2024-11-03 23:51:06 +0100

    Added santize check

--- Weather_Project_684006/Utilities/FileNameSanitizer.cs
+++ Weather_Project_684006/Utilities/FileNameSanitizer.cs
@@ -4,10 +4,15 @@ public class FileNameSanitizer
 {
     public static string Sanitize(string input)
     {
+        // Replace all spaces with dashes
+        input = input.Replace(' ', '-');
+
+        // Replace any invalid file name characters with dashes
         foreach (var c in Path.GetInvalidFileNameChars())
         {
             input = input.Replace(c, '-');
         }
+
         return input;
     }
 }
